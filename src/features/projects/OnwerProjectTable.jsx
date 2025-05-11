326e0178fe6615758fdfd7b5387e229ac2521bfb
Amin Shahin <aminshahin.web@gmail.com>
2025-05-11 09:41:19 +0330

    utils function added for owner projects table

--- src/features/projects/OnwerProjectTable.jsx
+++ src/features/projects/OnwerProjectTable.jsx
@@ -1,17 +1,20 @@
 import Empty from "../../ui/Empty";
 import Loader from "../../ui/Loader";
+import convertDateToLocalDate from "../../utils/convertDateToLocalDate";
+import { convertToPersianNumber, toPersianNumbersWithComma } from "../../utils/convertToPersianNumber";
+import toTruncateText from "../../utils/toTruncateText";
 import { useOwnerProject } from "./useOwnerProjects";
 
 function OnwerProjectTable() {
   const { isLoading, projects } = useOwnerProject();
   console.log(projects);
-  
+
   if (isLoading) return <Loader />;
   if (!projects.length) return <Empty />;
 
   return (
-    <div >
-      <table dir="rtl" className="shadow-lg" >
+    <div>
+      <table dir="rtl" className="shadow-lg">
         <thead>
           <tr className="title-row">
             <th>#</th>
@@ -29,13 +32,28 @@ function OnwerProjectTable() {
           {projects.map((project, index) => (
             <tr key={project._id}>
               <td>{index + 1}</td>
-              <td>{project.title}</td>
+              <td>{toTruncateText(project.title , 20)}</td>
               <td>{project.category.title}</td>
-              <td>{project.freelancer?.name || '-' }</td>
-              <td>{project.budget}</td>
-              <td>{project.deadline}</td>
-              <td>{project.status === 'OPEN' ? <span className="badge badge--success">باز</span> :<span className="badge badge--danger">بسته</span>}</td> 
-               <td className="flex flex-wrap gap-x-1.5">{project.tags.map(tag => <span key={tag} className="badge badge--secondary flex place-items-center">{tag}</span>)}</td>
+              <td>{project.freelancer?.name || "-"}</td>
+              <td>{toPersianNumbersWithComma(project.budget)+ ' ریال '}</td>
+              <td>{convertDateToLocalDate(project.deadline)}</td>
+              <td>
+                {project.status === "OPEN" ? (
+                  <span className="badge badge--success">باز</span>
+                ) : (
+                  <span className="badge badge--danger">بسته</span>
+                )}
+              </td>
+              <td className="flex flex-wrap gap-x-1.5">
+                {project.tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="badge badge--secondary flex place-items-center"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </td>
               <td>...</td>
             </tr>
           ))}
